2d4191e90287ebd93eff1402c396092d54ded27b
Youssef-Bedoui <Jozefbedoui@gmail.com>
2022-04-20 00:12:35 +0100

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/profile">Profile</router-link> |
+      <router-link to="/add">Add Event</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/sign up">Sign Up</router-link> |
+      <router-link to="/sign in">Sign in</router-link> |
+      <router-link to="/">Logout</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+     <Profile/>
+  </div>
+</template>
+
+<script>
+import Profile from "../components/Profile/Profile.vue"
+export default {
+    name:"ProfileView",
+    components:{
+        Profile 
+    }
+}
+</script>
+
+<style>
+
+</style>

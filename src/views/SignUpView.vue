a6e4022e00d6241d5eea0048a1c09d742eb5e507
unknown <brkhayreddine@gmail.com>
2022-04-21 03:02:03 +0100

    sign UP btn

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <SignUp />
+  </div>
+</template>
+
+<script>
+
+import SignUp from "@/components/Sign up/SignUp.vue"
+export default {
+  name: "SignUpView",
+  components: {
+    SignUp,
+  },
+};
+</script>
